57d5626084a9990123fffbbe53eb3026b973842f
Leo <tareuss@enschort.com>
2021-12-06 16:22:18 +0100

    restructuration 1 : pokedex national page, css

--- header.php
+++ header.php
@@ -6,29 +6,41 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link  rel="stylesheet" href="css.css">
+    <link  rel="stylesheet" href="assets/css.css">
 
 </head>
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
     <div class="container-fluid">
-        <a class="navbar-brand px-3" href="index.php">  <i class="fas fa-bomb"></i>  Nuzlocker Poke-plus-Dex </a>
+        <a class="navbar-brand px-3" href="pokedex.php">  <i class="fas fa-bomb"></i>  Nuzlocker Poke-plus-Dex </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarText">
 
 
+            <li class="nav-item">
+                <a class="nav-link" href="team.php"> Team</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="pokedex.php">National Pokedex</a>
+            </li>
 
 
-
-                <li class="nav-item dropdown ml-5">
+            <li class="nav-item dropdown ml-5">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         outil Session
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <li><a class="dropdown-item" href="varDumper.php">var dump session</a></li>
+
+                        <li class="nav-item">
+
+                        </li>
+
+
+
                         <li><a class="dropdown-item" href="dump.php">destroy session</a></li>
                     </ul>
                 </li>
@@ -36,7 +48,7 @@
 
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Meteo
+                       Pokedex par Version
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                         <li><a class="dropdown-item" href="meteo.php?temperature=-40&ville=Moscou">Moscou</a></li>
@@ -45,12 +57,8 @@
                     </ul>
                 </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="SucePark.php">Suce Park</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="choosingColor.php">Color Swaping</a>
-                </li>
+
+
 
                 <li class="nav-item">
                     <a class="nav-link" href="region.php">Régions</a>
--- pokedex.php
+++ pokedex.php
@@ -2,7 +2,7 @@
 require 'vendor/autoload.php';
 include 'header.php';
 include 'TraitAndService/GetDetail.php';
-$i=0;
+$i = 0;
 $url = 'https://pokeapi.co/api/v2/pokemon/';
 if (isset ($_GET['page'])) {
     $url .= $_GET['page'];
@@ -12,75 +12,88 @@ if (isset ($_GET['page'])) {
 $fullCall = file_get_contents($url);
 $pkmnList = json_decode($fullCall, true);
 ?>
-    <div class="container">
-        <div class="d-flex flex-column justify-content-center align-items-center">
-            <div class="row gx-3">
-                <?php
-                dump($pkmnList);
+
+    <div class="container-fluid position-relative">
+        <a> </a>
+        <a> </a>
+        <div class="row">
+            <div class="col-1 "></div>
+            <div class="col-10">
+
+                <div class="row">
+                    <div class="d-flex flex-column justify-content-center align-items-center">
+                        <div class="row gx-3">
+                            <?php
+                            dump($pkmnList);
 
 
-                foreach ($pkmnList
+                            foreach ($pkmnList
 
-                         as $result => $khey) {
+                                     as $result => $khey) {
 
-                    if ($result === 'next') {
+                                if ($result === 'next') {
 
-                        $rest = substr($khey, 34); ?>
-                        <div class="row flex-row-reverse">
-                        <div class="col-2 ">
-                            <a class="btn btn-info" href="index.php?page=<?php echo $rest ?>"> Next Page</a>
-                        </div>
-                    <?php }
-                    if ($result === 'previous') {
-                        $rest = substr($khey, 34); ?>
-                        <div class="col-2 me-auto">
-                            <a class="btn btn-info" href="index.php?page=<?php echo $rest ?>"> Previous Page</a>
-                        </div>
-                        </div>
-                    <?php } ?>
-
-
-                    <?php
-                    if ($result === 'results') {
-                        foreach ($khey
-
-                                 as $item) {
-                            ?>
-                            <div class="col-4 mb-3">
-                                <div class="card  shadow">
-                                    <div class="row g-0">
-                                        <div class="col-md-4">
-                                            <img src= <?php $var = new GetDetail();
-                                            $a = $var->recupDetail($item['url']);
-                                            $img = $a['sprites']['other']['official-artwork']['front_default'];
-                                            echo $img; ?>
-                                                 class="img-fluid rounded-start" alt="...">
-                                        </div>
-                                        <div class="col-md-8">
-                                            <div class="card-body">
-                                                <h5 style="text-transform: capitalize;"
-                                                    class="card-title"> <?php echo $item['name'] ?></h5>
-                                                <p class="card-text">
-                                                </p>
-                                                <p class="card-text"><small class="text-muted">Last updated 3 mins
-                                                        ago</small></p>
-                                                <a class="btn btn-info"> View Detail</a>
+                                    $rest = substr($khey, 34); ?>
+                                            <div <?php if($khey == null){echo 'class="d-none"';} ?>>
+                                        <a style="right:10px; top:40vh;" class="position-fixed" href="pokedex.php?page=<?php echo $rest ?>"><span class="nextPreviousArrow"><i class="fas fa-angle-right"></i></span></a>
                                             </div>
-                                        </div>
-                                    </div>
-                                </div>
+
+                                <?php }
+                                if ($result === 'previous') {
+                                    $rest = substr($khey, 34); ?>
+                            <div <?php if($khey == null){echo 'class="d-none"';} ?>>
+                                        <a  style="left:10px; top:40vh;" class="position-fixed" href="pokedex.php?page=<?php echo $rest ?>"><span class="nextPreviousArrow"><i class="fas fa-angle-left"></i></span></a>
                             </div>
 
-                            <?php
-                        }
+                                <?php } ?>
 
-                    }
-                } ?>
+
+                                <?php
+                                if ($result === 'results') {
+                                    foreach ($khey
+
+                                             as $item) {
+                                        ?>
+                                        <div class="col-4 mb-3">
+                                            <div class="card  shadow">
+                                                <div class="row g-0">
+                                                    <div class="col-md-4">
+                                                        <img src= <?php $var = new GetDetail();
+                                                        $a = $var->recupDetail($item['url']);
+                                                        $img = $a['sprites']['other']['official-artwork']['front_default'];
+                                                        echo $img; ?>
+                                                             class="img-fluid rounded-start" alt="...">
+                                                    </div>
+                                                    <div class="col-md-8">
+                                                        <div class="card-body">
+                                                            <h5 style="text-transform: capitalize;"
+                                                                class="card-title"> <?php echo $item['name'] ?></h5>
+                                                            <p class="card-text">
+                                                            </p>
+                                                            <p class="card-text"><small class="text-muted">Last updated
+                                                                    3 mins
+                                                                    ago</small></p>
+                                                            <a class="btn btn-info"> View Detail</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <?php
+                                    }
+
+                                }
+                            } ?>
+
+                        </div>
+                    </div>
+                </div>
 
             </div>
+            <div class="col-1"></div>
         </div>
     </div>
-
 <?php
 
 include 'footer.php';
